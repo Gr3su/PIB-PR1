54ba2a99b34d2189fac8d1ae4adca12e48c8c829
Yannick <yannickgross@outlook.de>
2022-12-15 21:22:54 +0100

    ueb07 soweit funktionierend

--- container/ueb07/LinkFilter.java
+++ container/ueb07/LinkFilter.java
@@ -9,8 +9,48 @@ import java.util.Scanner;
  */
 public class LinkFilter{
     
+    private LinkFilter(){}
+    
+    public static String linkFilter(){
+        final   String          LINK_INDIKATOR = "href";
+        final   Scanner         scanner         = new Scanner(System.in);
+        
+                int             filterStart     = -1;
+                int             filterEnde      = -1;
+                StringBuilder   ausgabe         = new StringBuilder();
+                String          link            = "";
+                String          hypertext       = "";
+                String          aktuelleZeile   = "";
+        
+        while(scanner.hasNextLine()){
+            
+            aktuelleZeile = scanner.nextLine();
+            filterStart = aktuelleZeile.indexOf(LINK_INDIKATOR);
+            
+            if(filterStart != -1){
+                
+                //Moegliche Leerzeichen zwischen "href" und Link-Start ueberspringen
+                filterStart = aktuelleZeile.indexOf("\"", filterStart) + 1;
+                
+                filterEnde = aktuelleZeile.indexOf("\"", filterStart);
+                link = aktuelleZeile.substring(filterStart, filterEnde).strip();
+                
+                //Moegliche Leerzeichen zwischen Link-Ende und Hypertext ueberspringen
+                filterEnde = aktuelleZeile.indexOf("\"", filterStart);
+                
+                filterStart = aktuelleZeile.indexOf(">", filterEnde) + 1;
+                filterEnde = aktuelleZeile.indexOf("<", filterStart);
+                hypertext = aktuelleZeile.substring(filterStart, filterEnde).strip();
+                
+                ausgabe.append(hypertext + "\t\t\t" + link + "\t\t" + link.length());
+                
+            }
+        }
+        
+        return ausgabe.toString();
+    }
+    
     public static void main(String [] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.next());
+        System.out.println(LinkFilter.linkFilter());
     }
 }

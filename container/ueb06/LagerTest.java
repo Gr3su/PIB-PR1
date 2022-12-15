a9f16f714e0d569c83d8f3696291a48bed3f8fff
Yannick <yannickgross@outlook.de>
2022-12-15 12:08:16 +0100

    ueb07 erstellt

--- container/ueb06/LagerTest.java
+++ container/ueb06/LagerTest.java
@@ -3,7 +3,6 @@ package container.ueb06;
 
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
---
+++ container/ueb07/LinkFilter.java
@@ -0,0 +1,16 @@
+package container.ueb07;
+import java.util.Scanner;
+
+/**
+ * Beschreiben Sie hier die Klasse LinkFilter.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class LinkFilter{
+    
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.next());
+    }
+}
